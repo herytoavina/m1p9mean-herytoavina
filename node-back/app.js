abbb1ecbad774e5883b8a985d4e78de7b3cecde0
herytoavina <aherytoavina@gmail.com>
2022-04-05 21:58:56 +0300

    Update app.js

--- node-back/app.js
+++ node-back/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://dbUser:dbUserPassword@cluster0.3wtmc.mongodb.net/ekaly?retryWrites=true&w=majority", {
-    useMongoClient: true
-})
+// mongoose.connect('mongodb+srv://dbUser:dbUserPassword@cluster0.3wtmc.mongodb.net/ekaly?retryWrites=true&w=majority',
+//     {
+//         useMongoClient: true
+//     }
+// );
+// mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
     res.status(200).json({
