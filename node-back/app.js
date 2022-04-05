34b49a4e499ee0b8acf67f36336a320348562036
herytoavina <aherytoavina@gmail.com>
2022-04-05 22:23:43 +0300

    Update app.js

--- node-back/app.js
+++ node-back/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (req, next) => {
+app.get("/", (req, res) => {
     res.json({message: "Hello World!"});
 });
+
+module.exports = app;
