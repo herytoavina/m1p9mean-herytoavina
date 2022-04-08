e4bdbea038413bd7f6653dae7bac20dace1dcc15
herytoavina <aherytoavina@gmail.com>
2022-04-08 15:35:57 +0300

    test

--- node-back/index.js
+++ node-back/index.js
@@ -1,36 +1,8 @@
-const express = require("express");
-const app = express();
-const userRoutes = require('./api/routes/user.route');
+const http = require('http');
+const app = require('./app');
 
+const port = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).json({});
-    }
-    next();
-  });
+const server = http.createServer(app);
 
-
-// parse requests of content-type - application/json
-app.use(express.json());
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-
-require('./api/routes/auth.route')(app);
-app.use('/api/user', userRoutes);
-
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
-});
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+server.listen(port);
