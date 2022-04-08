5f2ce8fb447c29bbc549839fefa44de3a3e4f0f3
herytoavina <aherytoavina@gmail.com>
2022-04-08 15:12:55 +0300

    Update app.js

--- node-back/app.js
+++ node-back/app.js
@@ -6,8 +6,8 @@ const mongoose = require("mongoose");
 
 
 const platRoutes = require('./api/routes/plat.route');
+const authRoutes = require('./api/routes/auth.routes');
 // const userRoutes = require('./api/routes/user.route');
-// const authRoutes = require('./api/routes/auth.routes');
 const dbConfig = require('./api/config/db.config');
 const db = require("./api/models");
 
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   });
 
 app.use('/api/plats', platRoutes);
-// app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 // app.use('/api/user', userRoutes);
 
 db.mongoose.connect(
