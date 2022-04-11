fb65475e104c536aa3ecc300dae13ecbdb0aa522
herytoavina <aherytoavina@gmail.com>
2022-04-11 23:46:57 +0300

    Update app.js

--- angular-front/app.js
+++ angular-front/app.js
@@ -1,12 +1,13 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
 
-app.use(express.static(__dirname + "/web"));
-app.use(express.static('dist/angular-front'));
+app.use(express.static(__dirname + '/dist/angular-front'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-front'}),
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+'/dist/angular-front/index.html'));
+});
 
-);
+app.listen(process.env.PORT || 8080);
 
-module.exports = app;
+module.exports = app
