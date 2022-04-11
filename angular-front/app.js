c8d606ddb1b2daa3d37ca062997231841a83be63
herytoavina <aherytoavina@gmail.com>
2022-04-11 23:38:55 +0300

    Update app.js

--- angular-front/app.js
+++ angular-front/app.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 
+app.use(express.static(__dirname + "/web"));
 app.use(express.static('dist/angular-front'));
 
 app.get('/*', (req, res) =>
